35e8f0716cef768adcef574dc1b413814974cfce
Thanapon Johdee <sern.dev@gmail.com>
2025-03-17 22:34:30 +0700

    refactor: remove unused

--- api/pkg/dto/problem.go
+++ api/pkg/dto/problem.go
@@ -1,12 +1,9 @@
 package dto
 
-import "mime/multipart"
-
 type ProblemRequestFrom struct {
-	Name         string                `form:"name" validate:"required,min=2,max=40"`
-	Description  string                `form:"description" validate:"required,omitempty"`
-	Language     []string              `form:"language" validate:"required,min=1"`
-	TestcaseNum  uint                  `form:"testcase_num" validate:"required,min=1"`
-	Zip          *multipart.FileHeader `form:"zip"`
-	EditableFile []string              `form:"editable_file" validate:"required"`
+	Name         string   `form:"name" validate:"required,min=2,max=40"`
+	Description  string   `form:"description" validate:"required,omitempty"`
+	Language     []string `form:"language" validate:"required,min=1"`
+	TestcaseNum  uint     `form:"testcase_num" validate:"required,min=1"`
+	EditableFile []string `form:"editable_file" validate:"required"`
 }
